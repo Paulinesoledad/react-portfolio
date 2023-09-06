333640bdec5c4b5050271830d1f19d07d41b02b5
Paulinesoledad <paulinesoledad1@gmail.com>
2023-09-06 11:05:16 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import ReactDOM from "react-dom";
+import React from "react";
+// import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client'
 import App from "./App";
 import  './index.css';
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+// ReactDOM.render(<App />, document.querySelector("#root"));
+createRoot(document.getElementById('root')).render(<App />)
